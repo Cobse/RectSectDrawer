58b721792b8d6a0d72ae2127ed3124b90eb4bdb9
Cobse <fredrik.jacobsen85@gmail.com>
2019-10-31 13:11:09 +0100

    Initial commit

---
+++ RectSectDrawer/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Codetesting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Picture1 = new System.Windows.Forms.Panel();
+            this.Text1 = new System.Windows.Forms.TextBox();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Picture1
+            // 
+            this.Picture1.BackColor = System.Drawing.Color.White;
+            this.Picture1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Picture1.Location = new System.Drawing.Point(300, 61);
+            this.Picture1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Picture1.Name = "Picture1";
+            this.Picture1.Size = new System.Drawing.Size(573, 289);
+            this.Picture1.TabIndex = 0;
+            // 
+            // Text1
+            // 
+            this.Text1.Location = new System.Drawing.Point(20, 26);
+            this.Text1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Text1.Name = "Text1";
+            this.Text1.Size = new System.Drawing.Size(76, 20);
+            this.Text1.TabIndex = 1;
+            this.Text1.TextChanged += new System.EventHandler(this.Text1_TextChanged);
+            // 
+            // dgv1
+            // 
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Location = new System.Drawing.Point(20, 93);
+            this.dgv1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.RowTemplate.Height = 24;
+            this.dgv1.Size = new System.Drawing.Size(180, 122);
+            this.dgv1.TabIndex = 2;
+            this.dgv1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv1_CellEndEdit);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(20, 61);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 401);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.Text1);
+            this.Controls.Add(this.Picture1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Panel Picture1;
+        public System.Windows.Forms.TextBox Text1;
+        public System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ RectSectDrawer/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Codetesting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            dgv1.AllowUserToAddRows = false;
+            dgv1.RowTemplate.Height = 16;
+
+
+            comboBox1.Items.Add("Rektangulært");
+            comboBox1.Items.Add("Polygon");
+            comboBox1.Items.Add("Diverse");
+
+
+        }
+
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            dgv1.Rows.Clear();
+            dgv1.Refresh();
+            dgv1.ColumnCount = 1;
+
+            tvType = comboBox1.Text;
+            switch (tvType)
+            {
+                case "Rektangulært":
+                    dgv1.RowCount = 2;
+
+                    dgv1.Rows[0].HeaderCell.Value = "A1";
+                    dgv1.Rows[1].HeaderCell.Value = "A2";
+                    break;
+                case "Polygon":
+                    dgv1.RowCount = 6;
+                    for (int i = 0; i < dgv1.RowCount; i++)
+                    {
+                        dgv1.Rows[i].HeaderCell.Value = "A" + (i + 1).ToString();
+
+                    }
+
+                    break;
+
+                case "Diverse":
+                    dgv1.RowCount = 3;
+
+                    dgv1.Rows[0].HeaderCell.Value = "A1";
+                    dgv1.Rows[1].HeaderCell.Value = "A2";
+                    dgv1.Rows[2].HeaderCell.Value = "A3";
+                    break;
+            }
+        }
+
+
+
+        public string tvType;
+
+        
+        //public string TvType
+        //{
+        //    get { return comboBox1.Text; }
+        //    // set { tvType = value; }
+        //}
+
+
+
+
+
+        private void dgv1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            
+            for (int i = 0; i < dgv1.RowCount; i++)
+            {
+
+                if (e.RowIndex == i)
+                {
+
+                    this.Picture1.Paint += Picture1_Paint;
+                    Picture1.Invalidate();
+
+                }
+
+
+            }
+
+        }
+
+
+        private void Picture1_Paint(object sender, PaintEventArgs e)
+        {
+
+            Draw(tvType ,Picture1, e);
+
+        }
+
+        private void Text1_TextChanged(object sender, EventArgs e)
+        {
+            //this.Picture1.Paint += Picture1_Paint;
+            Picture1.Refresh();
+            //Picture1.Invalidate();
+        }
+
+        public void Draw(string sectionType, Panel panel, PaintEventArgs e)
+        {
+           
+
+            var containerState = e.Graphics.BeginContainer();
+            Graphics graphics = e.Graphics;
+            
+            graphics.TranslateTransform(panel.Width / 2, panel.Height / 2);
+            Pen penDraw = new Pen(Color.Black, 1f);
+            Pen penDim = new Pen(Color.Red, 1f);
+            AdjustableArrowCap customArrowCap = new AdjustableArrowCap(5, 5);
+            List<string> tvSnittDimList = new List<string>();
+
+            float scale = 0.5f;
+            
+
+            float maxWidth = panel.Width - 200;
+            float maxHeight = panel.Height - 200;
+            float scaleWidth = 1;
+            float scaleHeight = 1;
+
+
+            
+            
+
+            switch (sectionType)
+            {
+                case "Rektangulært":
+                    int recWidth = Convert.ToInt32(dgv1[0, 0].Value);
+                    int recHeight = Convert.ToInt32(dgv1[0, 1].Value);
+
+                    if (recWidth > maxWidth)
+                    {
+                        scaleWidth = maxWidth/recWidth;
+                    }
+
+                    if (recHeight > maxHeight)
+                    {
+                        scaleHeight = maxHeight / recHeight;
+                    }
+
+
+                    scale = Math.Min(scaleWidth, scaleHeight);
+                    graphics.ScaleTransform(scale, scale);
+
+
+                    string strRecWidth = recWidth.ToString();
+                    string strRecHeight = recHeight.ToString();
+
+
+                    StringFormat strFormat = new StringFormat();
+                    strFormat.LineAlignment = StringAlignment.Center;
+                    strFormat.Alignment = StringAlignment.Center;
+
+
+
+                    //Make rectangel in center of panel
+
+                    graphics.DrawRectangle(penDraw, new Rectangle(-recWidth/2, -recHeight/2, recWidth, recHeight));
+
+                    //Make dimension lines
+                    penDim.CustomStartCap = customArrowCap;
+                    penDim.CustomEndCap = customArrowCap;
+                    Font dimFont = new Font("Arial", 12 / scale, FontStyle.Regular);
+
+                    graphics.DrawLine(penDim, -recWidth / 2, recHeight / 2 + 20 / scale, recWidth / 2, recHeight / 2 + 20 / scale);
+                    graphics.DrawLine(penDim, recWidth / 2 + 20 / scale, recHeight / 2, recWidth / 2 + 20 / scale, -recHeight / 2);
+                    graphics.DrawString(strRecWidth, dimFont, Brushes.Red, 0, recHeight / 2 + 40 / scale, strFormat);
+                    graphics.DrawString(strRecHeight, dimFont, Brushes.Red, recWidth / 2 + 50 / scale, 0, strFormat);
+                    
+
+                    break;
+
+                case "Polygon":
+
+                    List<PointF> pointList = new List<PointF>();
+
+                    PointF[] pointF = new PointF[] { };
+                    //PointF pointF;
+                    int j = 100;
+                    int k = 50;
+
+
+                    for (int i = 0; i < dgv1.RowCount; i++)
+                    {
+                        pointList.Add(new PointF(j, k));
+
+                        //pointF[i].X = j; //new Point { X = 0, Y = 0 };
+                        //pointF[i].Y = k;
+
+                        j += 50;
+                        k += Convert.ToInt32(Math.Pow(3.5, i));
+
+                    }
+
+                    pointF = pointList.ToArray();
+
+                    graphics.DrawPolygon(penDraw, pointF);
+                    //graphics.TranslateTransform(200, 0);
+                    graphics.DrawClosedCurve(penDraw, pointF);
+
+                    //graphics.TranslateTransform(-200, 0);
+
+                    break;
+
+                case "Diverse":
+                    ////FLip the Y-axis
+                    //graphics.ScaleTransform(1.0f, -1.0f);
+
+                    ////Transefer the drawing graphics accordingly
+                    //float height = panel.Height;
+                    //graphics.TranslateTransform(0.0f, -height);
+
+                    //Point[] points =
+                    //{
+                    //     new Point(10,  10),
+                    //     new Point(10, 100),
+                    //     new Point(200,  50),
+                    //     new Point(250, 300)
+
+                    //};
+
+                    //AdjustableArrowCap customArrowCap = new AdjustableArrowCap(6, 6);
+
+                    //pen.CustomEndCap = customArrowCap;
+                    //Point p1 = new Point(50, 50);
+                    //Point p2 = new Point(400, 200);
+
+
+                    //graphics.DrawLines(pen, points);
+                    //graphics.DrawLine(pen, p1, p2);
+                    //graphics.DrawString("1000", new Font("Verdana", 12), Brushes.Black, 100, 50);
+
+                    //graphics.EndContainer(containerstate);
+                    
+
+
+                    //String theString = "45 Degree Rotated Text";
+                    //SizeF sz = graphics.VisibleClipBounds.Size;
+                    ////Offset the coordinate system so that point (0, 0) is at the center of the desired area.
+                    //graphics.TranslateTransform(sz.Width / 2, sz.Height / 2);
+                    ////Rotate the Graphics object.
+                    //graphics.RotateTransform(45);
+                    //sz = graphics.MeasureString(theString, this.Font);
+                    ////Offset the Drawstring method so that the center of the string matches the center.
+                    //graphics.DrawString(theString, this.Font, Brushes.Black, -(sz.Width / 2), -(sz.Height / 2));
+                    //graphics.DrawString("1000", new Font("Verdana", 12), Brushes.Black, 100, 50);
+                    ////Reset the graphics object Transformations.
+                    //graphics.ResetTransform();
+
+
+                    break;
+            }
+            graphics.EndContainer(containerState);
+            penDraw.Dispose();
+            penDim.Dispose();
+
+
+
+        }
+
+        public void drawRotatedText(Panel panel, int x, int y, float angle, string text, Font font, Brush brush, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.TranslateTransform(x, y); // Set rotation point
+            g.RotateTransform(angle); // Rotate text
+            g.TranslateTransform(-x, -y); // Reset translate transform
+            SizeF size = g.MeasureString(text, font); // Get size of rotated text (bounding box)
+            g.DrawString(text, font, brush, new PointF(x - size.Width / 2.0f, y - size.Height / 2.0f)); // Draw string centered in x, y
+            g.ResetTransform(); // Only needed if you reuse the Graphics object for multiple calls to DrawString
+            g.Dispose();
+        }
+
+ 
+    }
+}
